537a18323d5f52a52cc7d182474e1147f6ec8ba8
Jason <jatsburgern@gmail.com>
2022-11-29 00:12:43 -0500

    added more config for render

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -16,6 +16,7 @@ def seed():
     if environment == 'production':
         # Before seeding in production, you want to run the seed undo
         # command, which will  truncate all tables prefixed with
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_friendships()
