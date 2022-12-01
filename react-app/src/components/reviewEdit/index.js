7c798019f8fe2e2fdc8742e5a8e9c5592b0b08a7
Jason <jatsburgern@gmail.com>
2022-12-01 10:57:58 -0500

    some styling

--- react-app/src/components/reviewEdit/index.js
+++ react-app/src/components/reviewEdit/index.js
@@ -1,41 +1,70 @@
-import { useState } from "react"
-import { editOneReview } from "../../store/review";
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { editOneReview, deleteOneReview } from "../../store/review";
+import { useEffect, useState } from "react"
+import { useHistory, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { editOneReview, getAllReviews, deleteOneReview } from "../../store/review";
 function ReviewEditForm() {
     const dispatch = useDispatch()
-    const [review, setReview] = useState("")
-    const [recommend, setRecommend] = useState(true)
-    const reviewId = useParams();
-    function submitReview() {
-        const payload = { content: review, rating: recommend, reviewId:reviewId }
-        dispatch(editOneReview(payload))
+    const history= useHistory()
+    // const [review, setReview] = useState("")
+    // const [recommend, setRecommend] = useState("")
+    const { reviewId } = useParams();
+
+    useEffect(() => {
+        dispatch(getAllReviews())
+    }, [dispatch])
+    const reviews = useSelector(state => state.review.reviews)
+    const reviewsArray= Object.values(reviews)
+    let oldReview;
+    console.log("REVIEW ID :", reviewId, reviews, reviewsArray)
+    if (reviews) {
+        console.log("REVIEWS :",reviews)
+        oldReview = reviews.find(review => +review.id === +reviewId)
     }
-    function deleteReview(){
+    console.log("OLD REVIEW :", oldReview)
+    // console.log(reviewsOld, "OLD DATA")
+
+
+
+    const [review, setReview] = useState(oldReview.review)
+    const [recommend, setRecommend] = useState(oldReview.recommend)
+    // if (reviews)
+    function editReview() {
+        const payload = { content: review, rating: recommend, reviewId: reviewId }
+        dispatch(editOneReview(payload))
+        history.push()
+    }
+    function deleteReview() {
         dispatch(deleteOneReview(reviewId))
+        history.push('/')
     }
     return (
 
-        <div> EDIT YOUR REVIEW!!!!
+        <div> EDIT REVIEW HERE!!!!
             <div className="review-input-container">
                 <label> YOUR REVIEW</label>
                 <input className="review-text"
-                    type="text"
+                    type="textarea"
                     value={review}
                     onChange={(e) => setReview(e.target.value)}
                     placeholder="TYPE YOUR REVIEW HERE"
                 />
                 <div className="review-recommend">
-                    <div className="recc-button" onClick={() => setRecommend(true)}>
-                        I LIKE THIS GAME
+                    <div className="question">
+
+                        Do you recommend this game?
+                        <div className="button-grouper">
+                            <div className={"recc-button"} onClick={() => setRecommend("like")}>
+                                Yes
+                            </div>
+                            <div className={"recc-button"} onClick={() => setRecommend("dislike")}>
+                                No
+                            </div>
+                        </div>
                     </div>
-                    <div className="recc-button"  onClick={() => setRecommend(false)}>
-                        I DISLIKE THIS GAME
-                    </div>
-                    <div className="review-submit" onClick={submitReview}> EDIT YOUR REVIEW</div>
+                    <div className="review-submit" onClick={editReview}> Edit Review</div>
+
                 </div>
-                <div className="review-submit" onClick={deleteReview}>DELETE THIS REVIEW?</div>
+                <div className="delete-review" onClick={deleteReview}>Delete your review</div>
             </div>
         </div>
     )
