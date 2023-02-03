08b3603b97195fbd211d2783f01de4ec0317bc98
Jason <jatsburgern@gmail.com>
2023-02-02 19:47:08 -0500

    begin importing friendships into messages page

--- react-app/src/components/messagePage/index.js
+++ react-app/src/components/messagePage/index.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 import React, { useEffect } from "react"
 import { getFriendMessages } from "../../store/message"
+import { getOneFriend } from "../../store/friend"
 import messageBox from "../messageBox"
 function MessagePage() {
 
@@ -9,10 +10,9 @@ function MessagePage() {
     const { friendshipId } = useParams()
     useEffect(() => {
         dispatch(getFriendMessages(friendshipId))
-
+        dispatch(getOneFriend(friendshipId))
     }, [])
     const messages = useSelector(state => state.messages.messages)
-    console.log("MESSAGES IN BOX:", messages)
     return (
         <div>
             MESSAGE BOX GOES HERE
