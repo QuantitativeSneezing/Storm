632fdb8770922c0d367cd87e734d65fc6274d82a
Jason <jatsburgern@gmail.com>
2023-02-23 19:14:34 -0500

    carousel begin, better styling

--- react-app/src/components/gameCarousel/index.js
+++ react-app/src/components/gameCarousel/index.js
@@ -1,9 +1,41 @@
-function GameCarousel(games){
-    const [currentGameLocation, setCurrentGameLocation]= useState(0)
+import { useState } from "react"
+import { useHistory } from "react-router-dom"
+function GameCarousel(games) {
+    games= games.games
+    const history= useHistory()
+    const [currentGameLocation, setCurrentGameLocation] = useState(0)
+    function incrementLocation(){
+        if (currentGameLocation>=games.length-1){
+            setCurrentGameLocation(0)
+        }else{
+            setCurrentGameLocation(currentGameLocation+1)
+        }
+    }
+    function decrementLocation(){
+        if (currentGameLocation==0){
+            setCurrentGameLocation(games.length-1)
+        }else{
+            setCurrentGameLocation(currentGameLocation-1)
+        }
+    }
+    let currentImage;
+    if(games){
+        if (games[currentGameLocation].images){
+            currentImage = games[currentGameLocation].images[0].url
+        }
+        }
+    // console.log("CURR IMAGE", currentImage)
+    function storePageRedirect() {
+            history.push(`/app/${games[currentGameLocation].id}`)
+    }
+    return (
+        <div className="carousel">
+            {/* {games && games[currentGameLocation].title} */}
+            <img src={currentImage} className="game-carousel-picture" onClick={storePageRedirect} alt="lifted straight from steam hosting lol"></img>
+            <div className="menu-button" onClick={incrementLocation}> NEXT GAME?</div>
+            <div className="menu-button" onClick={decrementLocation}> PREV GAME?</div>
 
-return (
-    <div className="carousel">
-
-    </div>
-)
+        </div >
+    )
 }
+export default GameCarousel
